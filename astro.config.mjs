5eeb17301c7de75b6a860f9f0592443045840914
Wystan  Wang <5812140+GrayOxygen@users.noreply.github.com>
2026-07-02 19:19:37 +0800

    fix: use @astrojs/tailwind integration in astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,11 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import tailwindcss from "@tailwindcss/vite";
-
+import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 
 export default defineConfig({
-  site: "https://sorolla.casset.cat/",
+  // site: "https://sorolla.casset.cat/",
   output: 'static',
-  integrations: [mdx(), sitemap(), icon()],
-  vite: {
-    plugins: [tailwindcss()],
-  },
+  integrations: [mdx(), sitemap(), icon(), tailwind()],
 });
